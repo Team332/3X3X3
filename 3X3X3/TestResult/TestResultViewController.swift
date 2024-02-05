001644a810495b7eb56e8d8aff548309be9f437e
y0unghyun <dl3rpika@gmail.com>
2024-02-05 17:44:55 +0900

    file created

---
+++ 3X3X3/TestResult/TestResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestResultViewController.swift
+//  3X3X3
+//
+//  Created by 영현 on 2/5/24.
+//
+
+import UIKit
+
+class TestResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
