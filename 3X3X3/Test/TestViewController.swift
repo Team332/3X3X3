b38e4a350c7854219e7c55d9fd7d4cfceb0e15b3
y0unghyun <dl3rpika@gmail.com>
2024-02-05 21:28:27 +0900

    backgroundcolor test

--- 3X3X3/Test/TestViewController.swift
+++ 3X3X3/Test/TestViewController.swift
@@ -12,6 +12,7 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
     
