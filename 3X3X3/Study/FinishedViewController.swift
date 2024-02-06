b04aa4bb9e29eb075bc56e070cbf20dfbda5ce9c
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-02-06 10:19:38 +0900

    [feat] FinishedViewContoller 추가, 기본 UI 구성, Assests 파일 추가

---
+++ 3X3X3/Study/FinishedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyFinishedViewController.swift
+//  3X3X3
+//
+//  Created by t2023-m0028 on 2/5/24.
+//
+
+import UIKit
+
+class StudyFinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
