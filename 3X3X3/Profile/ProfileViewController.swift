187a60769883a485b95dcb3ff28b41a2c86f8957
dahil4 <arhaxoda@naver.com>
2024-02-07 20:47:47 +0900

    [Fea] #4 프로필 UI 구현

--- 3X3X3/Profile/ProfileViewController.swift
+++ 3X3X3/Profile/ProfileViewController.swift
@@ -13,92 +13,142 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
-        view.addSubview(labelStack)
-        view.addSubview(scoreStack)
-        view.addSubview(expBar)
-        view.addSubview(expFullBar)
-
         setUi()
+        
+        //더미더미
+        User.shared.totalWords = 3324
+        updateExperience()
+        let calendarVC = CalendarViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        addChild(calendarVC)
+        view.addSubview(calendarVC.view)
+        
+        calendarVC.collectionView.snp.makeConstraints{ make in
+            make.top.equalTo(expStack.snp.bottom).offset(5)
+            make.leading.equalTo(15)
+            make.trailing.equalTo(-15)
+            make.bottom.equalTo(-90)
+        }
     }
     
+    func updateExperience() {
+        if let totalWords = User.shared.totalWords {
+            let expPercentage = min(Double(totalWords % 100) / 100.0, 1.0)
+            expBar.setProgress(Float(expPercentage), animated: true)
+            
+            totalWordsLabel.text = "\(totalWords) 개"
+            
+            let level = totalWords / 100
+            rankLabel.text = "\(level) 등급"
+            
+            let percentage = Int(expPercentage * 100)
+            expPercentLabel.text = "\(percentage) / 100"
+            expLabelText.text = "\(percentage)%"
+        }
+    }
+    
+    let samsamImage: UIImageView = {
+        let samsamImage = UIImageView()
+        samsamImage.translatesAutoresizingMaskIntoConstraints = false
+        samsamImage.contentMode = .scaleAspectFit
+        guard let image = UIImage(named: "332") else { return UIImageView() }
+        samsamImage.image = image
+        return samsamImage
+    }()
+    
     let totalWordsLabel: UILabel = {
-        let totalLabel = UILabel()
-        totalLabel.text = "9999"
-        totalLabel.font = UIFont.systemFont(ofSize: 17)
-        return totalLabel
+        let totalWordsLabel = UILabel()
+        totalWordsLabel.text = "9999"
+        totalWordsLabel.font = UIFont.systemFont(ofSize: 17)
+        return totalWordsLabel
     }()
     
     let averageScoreLabel: UILabel = {
-        let totalLabel = UILabel()
-        totalLabel.text = "9"
-        totalLabel.font = UIFont.systemFont(ofSize: 17)
-        return totalLabel
+        let averageScoreLabel = UILabel()
+        averageScoreLabel.text = "9"
+        averageScoreLabel.font = UIFont.systemFont(ofSize: 17)
+        return averageScoreLabel
     }()
     
     let rankLabel: UILabel = {
-        let totalLabel = UILabel()
-        totalLabel.text = "99"
-        totalLabel.font = UIFont.systemFont(ofSize: 17)
-        return totalLabel
+        let rankLabel = UILabel()
+        rankLabel.text = "99"
+        rankLabel.font = UIFont.systemFont(ofSize: 17)
+        return rankLabel
     }()
     
     let expLabelText: UILabel = {
-        let totalLabel = UILabel()
-        totalLabel.text = "88"
-        totalLabel.font = UIFont.systemFont(ofSize: 17)
-        return totalLabel
+        let expLabelText = UILabel()
+        expLabelText.text = "88"
+        expLabelText.font = UIFont.systemFont(ofSize: 17)
+        return expLabelText
     }()
-    
-    let scoreStack: UIStackView = {
-        let scoreStack = UIStackView()
-        scoreStack.axis = .vertical
-        scoreStack.spacing = 30
-        return scoreStack
-    }()
-    
     
     let totalLabel: UILabel = {
         let totalLabel = UILabel()
         totalLabel.text = "총단어수 :"
-        totalLabel.font = UIFont.systemFont(ofSize: 17)
+        totalLabel.font = UIFont.boldSystemFont(ofSize: 17)
         return totalLabel
     }()
     
     let averageLabel: UILabel = {
         let averageLabel = UILabel()
         averageLabel.text = "평균점수 :"
-        averageLabel.font = UIFont.systemFont(ofSize: 17)
+        averageLabel.font = UIFont.boldSystemFont(ofSize: 17)
         return averageLabel
     }()
     
     let tierLabel: UILabel = {
         let tierLabel = UILabel()
         tierLabel.text = "등       급 :"
-        tierLabel.font = UIFont.systemFont(ofSize: 17)
+        tierLabel.font = UIFont.boldSystemFont(ofSize: 17)
         return tierLabel
     }()
     
     let expLabel: UILabel = {
         let expLabel = UILabel()
         expLabel.text = "경  험  치 :"
-        expLabel.font = UIFont.systemFont(ofSize: 17)
+        expLabel.font = UIFont.boldSystemFont(ofSize: 17)
         return expLabel
     }()
     
     let expBar: UIProgressView = {
-       let expBar = UIProgressView()
+        let expBar = UIProgressView()
         expBar.trackTintColor = .clear
-        expBar.progressTintColor = UIColor(named: "Color")
-        expBar.setProgress(0.88, animated: true)
+        expBar.progressTintColor = .exp
+        expBar.setProgress(0.94, animated: true)
         return expBar
     }()
     
     let expFullBar: UIProgressView = {
-       let expBar = UIProgressView()
-        expBar.trackTintColor = .black
-//        expBar.setProgress(0.88, animated: true)
-        return expBar
+        let expFullBar = UIProgressView()
+        expFullBar.trackTintColor = .black
+        return expFullBar
+    }()
+    
+    let expPercentLabel: UILabel = {
+        let expPercentLabel = UILabel()
+        expPercentLabel.textAlignment = .right
+        expPercentLabel.font = UIFont.systemFont(ofSize: 12)
+        return expPercentLabel
+    }()
+    
+    let expStack: UIStackView = {
+        let expStack = UIStackView()
+        expStack.axis = .vertical
+        expStack.spacing = 3
+        return expStack
+    }()
+    
+    let scoreLabelStack: UIStackView = {
+        let scoreLabelStack = UIStackView()
+        scoreLabelStack.axis = .horizontal
+        scoreLabelStack.spacing = 10
+        scoreLabelStack.layer.cornerRadius = 15
+        scoreLabelStack.layoutMargins = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 40)
+        scoreLabelStack.isLayoutMarginsRelativeArrangement = true
+        scoreLabelStack.layer.masksToBounds = true
+        scoreLabelStack.backgroundColor = .team332
+        return scoreLabelStack
     }()
     
     let labelStack: UIStackView = {
@@ -108,15 +158,21 @@ class ProfileViewController: UIViewController {
         return labelStack
     }()
     
-    func setUi(){
-        labelStack.snp.makeConstraints { make in
-            make.top.equalTo(150)
-            make.left.equalTo(30)
-        }
-        scoreStack.snp.makeConstraints { make in
-            make.top.equalTo(150)
-            make.left.equalTo(120)
-        }
+    let scoreStack: UIStackView = {
+        let scoreStack = UIStackView()
+        scoreStack.axis = .vertical
+        scoreStack.spacing = 30
+        return scoreStack
+    }()
+    
+    func setUi() {
+        view.addSubview(samsamImage)
+        view.addSubview(scoreLabelStack)
+        view.addSubview(expStack)
+        
+        scoreLabelStack.addArrangedSubview(labelStack)
+        scoreLabelStack.addArrangedSubview(scoreStack)
+        
         labelStack.addArrangedSubview(totalLabel)
         labelStack.addArrangedSubview(averageLabel)
         labelStack.addArrangedSubview(tierLabel)
@@ -127,31 +183,24 @@ class ProfileViewController: UIViewController {
         scoreStack.addArrangedSubview(rankLabel)
         scoreStack.addArrangedSubview(expLabelText)
         
-        expBar.snp.makeConstraints { make in
-            make.top.equalTo(labelStack.snp.bottom).offset(40)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
+        expStack.addArrangedSubview(expBar)
+        expStack.addArrangedSubview(expFullBar)
+        expStack.addArrangedSubview(expPercentLabel)
+        
+        scoreLabelStack.snp.makeConstraints{ make in
+            make.top.equalTo(90)
+            make.left.equalTo(15)
         }
         
-        expFullBar.snp.makeConstraints { make in
-            make.top.equalTo(expBar.snp.bottom).offset(5)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
+        expStack.snp.makeConstraints { make in
+            make.top.equalTo(labelStack.snp.bottom).offset(40)
+            make.leading.equalTo(15)
+            make.trailing.equalTo(-15)
+        }
+        
+        samsamImage.snp.makeConstraints{ make in
+            make.top.equalTo(80)
+            make.right.equalTo(-35)
         }
     }
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
