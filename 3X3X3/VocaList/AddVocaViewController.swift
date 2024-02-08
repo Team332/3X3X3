6dbe99df3b920bd1f4b41e840a024c9571708c54
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-02-08 16:39:47 +0900

    [Chore] 코어데이터 import추가

--- 3X3X3/VocaList/AddVocaViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AddVocaViewController.swift
-//  3X3X3
-//
-//  Created by t2023-m0028 on 2/7/24.
-//
-
-import Foundation
--- 3X3X3/VocaList/VocaListCollectionCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  VocalistCollectionCell.swift
-//  3X3X3
-//
-//  Created by t2023-m0028 on 2/7/24.
-//
-
-import Foundation
