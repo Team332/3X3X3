d2025dbf34801e706f89e66eba90f81486fcb74d
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-02-15 20:27:18 +0900

    [Del] viewWillAppear 코드 & 미사용 주석 삭제

--- 3X3X3/TestResult/TestResultViewController.swift
+++ 3X3X3/TestResult/TestResultViewController.swift
@@ -32,24 +32,9 @@ class TestResultViewController: UIViewController, UICollectionViewDataSource, UI
         createCircle()
         setupUI()
         setupConstraint()
-//        sendData()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        //        sendData()
-    }
-    
-
 
 // MARK: - Data Setup
-//    private func sendData() {
-//        if let profileViewController = navigationController?.viewControllers.first(where: { $0 is ProfileViewController }) as? ProfileViewController {
-//            profileViewController.correctRate = correctRate
-//        }
-//
-//    }
 
     private func setupData() {
         setupIncorrectWord()
